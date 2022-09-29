fc857d646eb3e4fefee17ea920935ea437fee1e7
Sebastian Hens <sebastian.hens@gmail.com>
2022-09-29 16:47:53 +0200

    feat(MCD-31): added category api calls

--- curseforge/api/categories.go
+++ curseforge/api/categories.go
@@ -1,1 +1,31 @@
+// ----------------
+// Games API @see https://docs.curseforge.com/?go#curseforge-core-api-categories
+// ----------------
+
 package api
+
+import (
+	"github.com/lyssar/msdcli/curseforge/schemas"
+	"net/url"
+	"strconv"
+)
+
+const (
+	UriCategories schemas.ApiUri = "/v1/categories"
+)
+
+// GetCategories @see https://docs.curseforge.com/?go#get-categories
+func GetCategories(gameID int, classID *int) (response schemas.GetCategoriesResponse, err error) {
+	q := url.Values{}
+	q.Add("gameId", strconv.Itoa(gameID))
+	if classID != nil {
+		q.Add("classId", strconv.Itoa(*classID))
+	}
+
+	curseforgeClient := NewCurseforgeClientForRoute(string(UriCategories))
+	curseforgeClient.Query(&q)
+	curseforgeClient.Request()
+	err = curseforgeClient.GetContent(&response)
+
+	return
+}
